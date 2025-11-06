9485ae688e54d0c554b8dfeb66975ceb8ed14caf
tasoula <nastiakorotkikh@yandex.ru>
2025-11-06 11:39:17 +0300

    1. создана структура многомодульного проекта 2. добавлен подпроект front-ui

---
+++ services/front-ui/src/main/java/io/github/tasoula/front_ui/FrontUiApplication.java
@@ -0,0 +1,13 @@
+package io.github.tasoula.front_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontUiApplication.class, args);
+	}
+
+}
---
+++ services/front-ui/src/test/java/io/github/tasoula/front_ui/FrontUiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tasoula.front_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
