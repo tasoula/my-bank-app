30a4e27843629311631cc0728e13a3c23013b87a
tasoula <nastiakorotkikh@yandex.ru>
2025-11-27 23:35:46 +0300

    Настройка логаута

--- microservices/front-ui/src/main/java/io/github/tasoula/front_ui/config/SecurityConfig.java
+++ microservices/front-ui/src/main/java/io/github/tasoula/front_ui/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
@@ -20,6 +22,8 @@ import org.springframework.security.web.server.authorization.HttpStatusServerAcc
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 
 import java.net.URI;
@@ -28,6 +32,8 @@ import java.net.URI;
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
+
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
@@ -49,9 +55,9 @@ public class SecurityConfig {
                         .authenticationSuccessHandler(successHandler())
                 )
                 // Настройка ручки логаута (автоматически чистит сессию)
-             //   .logout(logout -> logout
-              //          .logoutSuccessHandler(oidcLogoutSuccessHandler())
-             //   )
+                .logout(logout -> logout
+                        .logoutSuccessHandler(logoutSuccessHandler())
+                )
              //   .anonymous(anonymous -> anonymous
             //            .principal("guestUser")
             //            .authorities("ROLE_GUEST")
@@ -74,6 +80,75 @@ public class SecurityConfig {
         };
     }
 
+
+    private final ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfig(ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    // Замените ваш текущий private метод logoutSuccessHandler() на этот бин:
+    @Bean
+    public ServerLogoutSuccessHandler logoutSuccessHandler() {
+        OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(this.clientRegistrationRepository);
+
+        // Указываем, куда вернуться после того, как Keycloak завершит свою сессию
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:8080/");
+
+        return oidcLogoutSuccessHandler;
+    }
+
+ /*   @Bean
+    public ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedServerLogoutSuccessHandler logoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(this.clientRegistrationRepository);
+
+        // Указываем URI, на который пользователь будет перенаправлен после
+        // завершения сессии в Keycloak и в вашем приложении.
+        // Здесь используется значение по умолчанию "{baseUrl}", которое обычно
+        // разрешается в корень вашего приложения, например, "http://localhost:8080/".
+        logoutSuccessHandler.setPostLogoutRedirectUri("/");//(URI.create("/"));
+
+        return logoutSuccessHandler;
+    }
+
+  */
+
+ /*   @Bean
+    public ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        // Замените этот URL на актуальный URL вашего Keycloak сервера
+        // В продакшене лучше получать issuer-uri из настроек application.yml
+        URI logoutUri = URI.create("http://localhost:8082/realms/master/protocol/openid-connect/logout");
+
+     /*    OidcClientInitiatedServerLogoutSuccessHandler handler = new OidcClientInitiatedServerLogoutSuccessHandler(
+                // Использует бин ReactiveClientRegistrationRepository, который Spring Boot предоставляет автоматически
+                clientRegistrationRepository
+        );
+        handler.setPostLogoutRedirectUri("/main");
+        return handler;
+
+        */
+
+        // Альтернативный, более ручной подход для формирования URI выхода:
+
+        /*     return (exchange, authentication) -> Mono.defer(() -> {
+            // Применяем параметры для перенаправления обратно в приложение после выхода из Keycloak
+            URI location = UriComponentsBuilder.fromUri(logoutUri)
+                    .queryParam("client_id", "front-ui") // Ваш client-id из application.yml
+                    .queryParam("post_logout_redirect_uri", "http://localhost:8080/") // Куда вернуться
+                    .build()
+                    .toUri();
+
+            exchange.getResponse().setStatusCode(HttpStatus.SEE_OTHER);
+            exchange.getResponse().getHeaders().setLocation(location);
+            return exchange.getResponse().setComplete();
+        });
+
+         */
+
+ //   }
+
   /*  @Bean
     public ServerLogoutSuccessHandler logoutSuccessHandler() {
         return (exchange, authentication) -> {
@@ -85,12 +160,6 @@ public class SecurityConfig {
     }
 */
 
-    // Обработчик выхода из системы (Logout handler)
-    // В реальном приложении нужно также отправлять запрос на завершение сессии в Keycloak (RP-initiated logout)
- /*   private ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
-        // Это пример, требующий доработки для взаимодействия с OIDC провайдером
-        return new HttpStatusCollectingServerLogoutSuccessHandler(HttpStatus.SEE_OTHER);
-    }
 
  /*    @Bean
     public ServerSecurityContextRepository securityContextRepository() {
