1f10700043ee785012fc31e5b45d14c53d182647
tasoula <nastiakorotkikh@yandex.ru>
2025-11-28 22:39:21 +0300

    1. Добавлена структура API Gateway

---
+++ microservices/api-gateway/src/main/java/io/github/tasoula/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package io.github.tasoula.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
